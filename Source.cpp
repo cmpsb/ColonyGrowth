abacd60b25ad1b37a46b453456846018e9bddde0
Duco <ducovanholthe@gmail.com>
2017-02-27 15:54:05 +0100

    Nieuwe versie, was blijkbaar oude versie met bugs. Nu werkt alles goed in de nieuwe opmaak van Romano. Ook 3x zo snel geworden.

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,7 @@
 - Aren't you looping over neighbours twice?
 - p array vs p vector speed
 - Local vs global p
+- why cstdio?
 */
 
 #include <iostream>
@@ -12,7 +13,7 @@
 #include <vector>
 #include <string>
 #include "print.h"
-#include <cstdio>
+//#include <cstdio>
 
 using namespace std;
 const double pi = 4 * atan(1);
@@ -63,7 +64,7 @@ struct Particle
 	vector<int> neighbours;
 
 	int ID;
-	double D, L, mu, Lmax; //size properties: diameter, length, growth rate
+	double D, L, len, mu, Lmax; //size properties: diameter, restlength, actual length, growth rate, max length
 	double xcpos, ycpos, theta; //spatial variables
 	double x1pos, y1pos, x2pos, y2pos, xvel1, yvel1, xvel2, yvel2, Fx1, Fy1, Fx2, Fy2; //more spatial variables
 	double oop; int ram; //sets orientational order parameter and defect as property of particle
@@ -75,6 +76,7 @@ Particle::Particle(double xstart, double ystart, double angle, double length, do
 	ID = 0;
 	D = diameter;
 	L = length; //rest length of particle (internal spring is at rest)
+	len = L;
 	mu = growth; //initial growth rate of particle
 	Lmax = randomLmax();
 
@@ -87,7 +89,7 @@ Particle::Particle(double xstart, double ystart, double angle, double length, do
 	Fx1 = 0.0; Fy1 = 0.0; Fx2 = 0.0; Fy2 = 0.0;
 
 	oop = 0.0; ram = 0;
-	colour = 240; //used for visualisation
+	colour = 200; //used for visualisation
 }
 
 void Particle::grow()
@@ -105,7 +107,7 @@ void Particle::forceInternal()
 {
 	double f_x = 0.0; double f_y = 0.0;
 	double dx = x1pos - x2pos; double dy = y1pos - y2pos;
-	double len = sqrt(dx*dx + dy*dy); //compute distance between discs to determine spring force
+	len = sqrt(dx*dx + dy*dy); //compute distance between discs to determine spring force
 
 	f_x = -ki * (len - L) * (dx / len);
 	f_y = -ki * (len - L) * (dy / len);
@@ -135,13 +137,20 @@ void Particle::clear()
 
 void divide(Particle &pOld, Particle &pNew)
 {
-	pNew.L = (pNew.L - pNew.D) / 2;
-	pNew.theta += randomTheta();
-	pNew.x1pos = pOld.x2pos - pNew.L * cos(pNew.theta); pNew.y1pos = pOld.y2pos - pNew.L * sin(pNew.theta);
-	pNew.mu += randomMu();
+	double oldL = pOld.L;
+	double oldDx = pOld.x2pos - pOld.x1pos; double oldDy = pOld.y2pos - pOld.y1pos;
 
-	pOld.L = (pOld.L - pOld.D) / 2;
-	pOld.x2pos = pOld.x1pos + pOld.L * cos(pOld.theta); pOld.y2pos = pOld.y1pos + pOld.L * sin(pOld.theta);
+	pNew.L = (oldL - pNew.D) / 2; //update new positions daughter cell
+	pNew.x2pos = pOld.x2pos; pNew.y2pos = pOld.y2pos; //needed to make sure positions are chosen in right direction
+	pNew.x1pos = pNew.x2pos - (oldDx * pNew.L / oldL); pNew.y1pos = pNew.y2pos - (oldDy * pNew.L / oldL);
+
+	double dtheta = randomTheta(); //give daughter cell new orientation (noise)
+	pNew.x2pos += dtheta * sin(pNew.theta); pNew.y2pos += dtheta * cos(pNew.theta);
+
+	pNew.mu += randomMu(); //change growthrate of dauhter cell
+
+	pOld.L = (oldL - pOld.D) / 2; //update new positions mother cell
+	pOld.x2pos = pOld.x1pos + (oldDx * pOld.L / oldL); pOld.y2pos = pOld.y1pos + (oldDy * pOld.L / oldL);
 	pOld.mu += randomMu();
 }
 
@@ -174,7 +183,7 @@ void distForce(Particle &p1, Particle &p2)
 	double d1 = 0.0, d2; //relative point [0,1] on line segments of particles to determine closest distance, d1 starts as 0 when lines are parallel
 
 						 //calculate dot products, s# corresponds to line segment of particle #, r is vector between first point of s1, s2
-	double s1s1 = p1.L*p1.L; double s2s2 = p2.L*p2.L;
+	double s1s1 = p1.len*p1.len; double s2s2 = p2.len*p2.len;
 	double s1s2 = (p1.x2pos - p1.x1pos) * (p2.x2pos - p2.x1pos) + (p1.y2pos - p1.y1pos) * (p2.y2pos - p2.y1pos);
 	double s1r = (p1.x2pos - p1.x1pos) * (p1.x1pos - p2.x1pos) + (p1.y2pos - p1.y1pos) * (p1.y1pos - p2.y1pos);
 	double s2r = (p1.x1pos - p2.x1pos) * (p2.x2pos - p2.x1pos) + (p1.y1pos - p2.y1pos) * (p2.y2pos - p2.y1pos);
@@ -228,9 +237,8 @@ void growAll(vector<Particle> &p, int nop)
 	{
 		p[g].neighbours.clear(); //clear neighbours vector
 		p[g].grow();
-		if (p[g].L > p[g].Lmax) //division
+		if (p[g].len > p[g].Lmax) //division, choose restlength (L) or actual Length (len)
 		{
-			p[g].L = (p[g].x2pos - p[g].x1pos) / cos(p[g].theta); //Check if legal!! Set new length for dividing
 			p.push_back(Particle(p[g].x1pos, p[g].y1pos, p[g].theta, p[g].L, p[g].D, p[g].mu)); //new particle is made with same properties as mother
 			divide(p[g], p[p.size() - 1]); //function that sets new properties to daughter particles
 			p[p.size() - 1].ID = p.size() - 1; //set ID of new particle
@@ -238,9 +246,9 @@ void growAll(vector<Particle> &p, int nop)
 	}
 }
 
-void printAll(vector<Particle> &p, Print &myPrint)
+void printAll(vector<Particle> &p, Print &myPrint, int nop)
 {
-	for (int w = 0; w < p.size(); w++)
+	for (int w = 0; w < nop; w++)
 	{
 		myPrint.print_data_js(p[w].x1pos, p[w].y1pos, p[w].x2pos, p[w].y2pos, p[w].D / 2, p[w].colour, p[w].ID);
 	}
@@ -249,10 +257,9 @@ void printAll(vector<Particle> &p, Print &myPrint)
 void moveAll(vector<Particle> &p, int nop)
 {
 	double distx, disty, range;
-	for (int m = 0;m < nop;m++) //second loop for movement of particles
+	for (int m = 0; m < nop; m++) //second loop for movement of particles
 	{
-		p[m].colour = 240; //before finding defects set colour to initial value, placed here for convenience
-		for (int nb = m+1 ;nb < nop;nb++)
+		for (int nb = m+1; nb < nop; nb++)
 		{
 			distx = (p[m].xcpos - p[nb].xcpos) * (p[m].xcpos - p[nb].xcpos);
 			disty = (p[m].ycpos - p[nb].ycpos) * (p[m].ycpos - p[nb].ycpos);
@@ -271,29 +278,21 @@ void analyzeAll(vector<Particle> &p, int nop, Print &myPrint, int save, int ts)
 	int amonb; //amount of neighbours
 	double sumsop; //sum of sop over triangles
 	int notr; //number of triangles
-	for (int d = 0;d < nop;d++) //third loop to find defects
+	for (int d = 0; d < nop; d++) //third loop to find defects
 	{
 		amonb = p[d].neighbours.size();
-		//double sumcosThetapn = 0.0; //set sum of angles between particle and its neighbours for sop
-		//double sumsinThetapn = 0.0;
 		sumsop = 0.0;
 		notr = 0;
+
 		if (amonb > 1) //if using triangles than amonb > 1
 		{
-			for (int a = 0;a < amonb;a++) //loop over neighbours
+			for (int a = 0; a < amonb; a++) //loop over neighbours
 			{
 				//use to determine oop or sop
 				int nb1 = p[d].neighbours[a]; //ID of neighbour 1
 				int nonb1 = p[nb1].neighbours.size(); //set number of neighbours of nb1
-				//double thetapn = sqrt((p[d].theta - p[nb1].theta) * (p[d].theta - p[nb1].theta)); //absolute angle between particle and neighbour
-				//if (thetapn > (pi / 2))
-				//	thetapn = pi - thetapn;
-				//double costhetapn = cos(2 * p[nb1].theta); //use for sop with all neighbours
-				//double sinthetapn = sin(2 * p[nb1].theta);
-				//sumcosThetapn += costhetapn;
-				//sumsinThetapn += sinthetapn;
 
-				for (int b = 0;b < amonb;b++) //use for finding defects with rotational angle method
+				for (int b = 0;b < amonb;b++) //use loop for finding defects with rotational angle method
 				{
 					if (b > a)
 					{
@@ -311,9 +310,9 @@ void analyzeAll(vector<Particle> &p, int nop, Print &myPrint, int save, int ts)
 							double theta12 = (p[d].theta - p[nb1].theta) * (p[d].theta - p[nb1].theta);
 							double theta23 = (p[nb1].theta - p[nb2].theta) * (p[nb1].theta - p[nb2].theta);
 							double theta31 = (p[nb2].theta - p[d].theta) * (p[nb2].theta - p[d].theta);
-							double ts = (pi / 2) * (pi / 2);
+							double ta = (pi / 2) * (pi / 2);
 							//if only one angle of the three is larger than pi/2 than there is a defect
-							if ((theta12 < ts && theta23 < ts && theta31 > ts) || (theta23 < ts && theta31 < ts && theta12 > ts) || (theta12 < ts && theta31 < ts && theta23 > ts))
+							if ((theta12 < ta && theta23 < ta && theta31 > ta) || (theta23 < ta && theta31 < ta && theta12 > ta) || (theta12 < ta && theta31 < ta && theta23 > ta))
 							{
 								p[d].ram = 1; //defect
 							}
@@ -325,17 +324,15 @@ void analyzeAll(vector<Particle> &p, int nop, Print &myPrint, int save, int ts)
 					}
 				}
 			}
-			//p[d].oop = sumThetapn / (amonb * (pi / 2)); //gives orientational order parameter between 0 and 1
-			//p[d].oop = 1 - 2 * sqrt((sumcosThetapn / (2 * amonb)) * (sumcosThetapn / (2 * amonb)) + (sumsinThetapn / (2 * amonb)) * (sumsinThetapn / (2 * amonb))); //gives sop from 0 to 1 for all nb
 			if (notr > 0)
-				p[d].oop = sumsop / notr; //gives sop from 0 to 1 for triangle
+				p[d].oop = sumsop / notr; //gives sop from 0 to 1 for triangles
 		}
 		if (p[d].ram == 1)
-			p[d].colour = 120;
+			p[d].colour = 10;
 		else
-			p[d].colour = (int)(p[d].oop * 160 + 200);
+			p[d].colour = (int)(p[d].oop * 200 + 180);
 
-		if (ts % (4 * save) == 0)
+		if (ts % save == 0)
 			myPrint.print_data_oop(ts, p[d].ID, p[d].xcpos, p[d].ycpos, p[d].theta, p[d].L, p[d].oop, p[d].ram); //save densities, in cluster 8 inputs are needed
 
 		p[d].ram = 0; //reset defect after printing
@@ -349,16 +346,20 @@ int main()
 	string dataNumber = "0123456789abcdefghij"; //able to create up to 20 folders during one simulation
 	int nop; //number of particles is one at start
 	int ts; //timestep
-	int save = 500; //determines how many times data is saved, for nop = 3000 time runs till 650000. 400 gives nice set of data
-	for (int loop = 0;loop < 1;loop++) //multiple loops to gather data, set number after loop < ..., to set number of data folders
+	int save = 400; //determines how many times data is saved, for nop = 3000 time runs till 650000. 400 gives nice set of data
+
+	int startRound = 0;
+	int noRounds = 1; //number of rounds
+	for (int round = startRound; round < (startRound + noRounds); round++) //multiple rounds to gather data, set number after round < ..., to set number of data folders
 	{
-		cout << "Type of sim: loop nr = " << loop << ", nop = " << Nmax << endl;
+		cout << "Type of sim: loop nr = " << round << ", nop = " << Nmax << endl;
 		cout << "Mu = " << growthRate / relaxTime << ", muDev = " << growthRateDev << ", L/D = " << maxLoverD << ", Ldev = " << maxLengthDev << ", k_i = " << ki / tau << ", k_o = " << ko / tau << endl;
-		cout << "DF = RAM+SOPTR." << ", ON = " << orientNoise << endl;
+		cout << "DF = RAM+SOPTR" << ", ON = " << orientNoise << endl;
+
 		nop = 1;
 		ts = 0;
 
-		string nameData(1, dataNumber[loop]); //setup printing for visualisation and data writing
+		string nameData(1, dataNumber[round]); //setup printing for visualisation and data writing
 		Print print;
 		print.init(nameData, nop);
 		print.N = nop;
@@ -371,18 +372,18 @@ int main()
 				growAll(p, nop); // t < 1 ns
 
 			nop = p.size(); //determine new number of particles after growth
-
 			if (ts % save == 0)
-				print.N = nop;
-
-			if (ts % (save * 10) == 0) //printing now separate from the movement
-				printAll(p, print);
+				print.N = nop; //update nop after growth and division
 
 			moveAll(p, nop); //find forces, velocities and movement of particles
 
-			analyzeAll(p, nop, print, save, ts); //analyze system to find order parameter and defects
+			if (ts % save == 0)
+				analyzeAll(p, nop, print, save, ts); //analyze system to find order parameter and defects
 
-			if (ts % 10000 == 0)
+			if ((round == startRound) && (ts % (save * 8) == 0))
+				printAll(p, print, nop); //printing for visualisation
+
+			if (ts % (save * 20) == 0)
 				cout << "At time " << ts << " nop is " << nop << endl;
 
 			ts++;
